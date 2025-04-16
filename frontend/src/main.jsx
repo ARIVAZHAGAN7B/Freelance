d54971ccb7dd01301e1481fd2e0f53bc2f0f369d
ARIVAZHAGAN7B <barivazhagan7@gmail.com>
2025-04-16 07:30:54 +0530

    added scrolltop.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
+import ScrollTop from './ScrollTop.jsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
+    <ScrollTop />
       <App />
     </BrowserRouter>
   </React.StrictMode>
