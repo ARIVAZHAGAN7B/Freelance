7411c9d6dda9ffdd68d49ca63f6334ff40256595
ARIVAZHAGAN7B <barivazhagan7@gmail.com>
2025-04-17 11:32:09 +0530

    added mongodb connection string

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,21 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
+const connectDb = require("./config/db"); 
+require("dotenv").config(); // Load .env
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+// MongoDB Connection
+connectDb();
+// Routes
 const CrewRoute = require("./Routes/Crew/CrewRoute");
-
 app.use("/api/crew", CrewRoute);
 
+// Start Server
 app.listen(5000, () => {
-  console.log("Server is running on port 5000");
+  console.log("🚀 Server is running on port 5000");
 });
